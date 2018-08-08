2246df2e26943666e1697d3432c2951c503c30ac
kavee <kaveepart@gmail.com>
2018-08-08 11:22:42 +0700

    initial files

--- app/src/main/java/com/internship/nilecon/cartels/SplashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/internship/nilecon/cartels/SplashScreen/SplashScreenActivity.kt
@@ -1,13 +1,45 @@
 package com.internship.nilecon.cartels.SplashScreen
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.internship.nilecon.cartels.Authentication.AuthenticationActivity
 import com.internship.nilecon.cartels.R
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    private var handler: Handler? = null
+    private var runnable: Runnable? = null
+    private var delay_time: Long = 0
+    private var time = 1500L
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+
+        runnable = Runnable {
+            val intent = Intent(this, AuthenticationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        delay_time = time
+        handler!!.postDelayed(runnable, delay_time)
+        time = System.currentTimeMillis()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        handler!!.removeCallbacks(runnable)
+        time = delay_time - (System.currentTimeMillis() - time)
     }
 }
