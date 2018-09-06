1448e4f9d54c870448f6c8882dce54072387b850
kavee <kaveepart@gmail.com>
2018-09-06 13:54:01 +0700

    initial files

---
+++ app/src/main/java/com/internship/nilecon/cartels/ParkingDetail/ParkingDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.internship.nilecon.cartels.ParkingDetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.internship.nilecon.cartels.R
+
+class ParkingDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parking_detail)
+    }
+}
